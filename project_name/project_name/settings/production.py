9abb61b0894165d0c98745b0e105e08065c8e9ab
taelimoh <taelimoh@gluwa.com>
2014-11-10 04:24:02 +0900

    added TODO for settings/production.py

--- project_name/project_name/settings/production.py
+++ project_name/project_name/settings/production.py
@@ -53,6 +53,7 @@ SERVER_EMAIL = EMAIL_HOST_USER
 ########## END EMAIL CONFIGURATION
 
 ########## DATABASE CONFIGURATION
+# TODO: Enter your application id below. If you have signed up
 if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
     # Running on production App Engine, so use a Google Cloud SQL database.
     DATABASES = {
